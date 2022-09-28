cce689e3c186d5425e79613a66e6a3567d3f55c5
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-09-28 18:12:16 +0500

    initialized and firebase.js setup

---
+++ whatsapp_clone/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDdjhgsMeu_ToSKLsbVgGlwFZOxyg-uRAA",
+    authDomain: "whatsapp-react-e4240.firebaseapp.com",
+    projectId: "whatsapp-react-e4240",
+    storageBucket: "whatsapp-react-e4240.appspot.com",
+    messagingSenderId: "873295405489",
+    appId: "1:873295405489:web:9bca8fb8eed6feb0761d90",
+    measurementId: "G-B8V6FNBM15"
+};
