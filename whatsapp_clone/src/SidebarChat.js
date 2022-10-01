bf155ad76553c6ee089cda931e8c5f82b30e2bfd
Ghost620 <79418234+Ghost620@users.noreply.github.com>
2022-10-01 15:36:18 +0500

    last message

--- whatsapp_clone/src/SidebarChat.js
+++ whatsapp_clone/src/SidebarChat.js
@@ -7,6 +7,14 @@ import { Link } from "react-router-dom";
 const SidebarChat = ( { id, name, addNewChat } ) => {
 
   const [seed, setSeed] = useState('')
+  const [messages, setMessages] = useState('')
+
+  useEffect(() => {
+    if (id) {
+      db.collection('rooms').doc(id).collection('messages').orderBy('timestamp', 'desc').onSnapshot( (snap) => setMessages(snap.docs.map( (doc) => doc.data())));
+    }
+  }, []);
+
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000))
   }, [])
@@ -27,7 +35,7 @@ const SidebarChat = ( { id, name, addNewChat } ) => {
           <Avatar src={`https://avatars.dicebear.com/api/bottts/:${seed}.svg`}/>
           <div className='sidebarChat_info'>
               <h2>{name}</h2>
-              <p>Last message on the room</p>
+              <p>{messages[0]?.message}</p>
           </div>
       </div>
     </Link>
